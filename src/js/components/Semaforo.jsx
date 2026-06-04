77ab9c78de0e60672dade15ddd5c055c4a977888
Juan <jdondayr@gmail.com>
2026-06-04 08:54:53 +0200

    added new features: 1) Creation of purple light by clicking a button, including it on the alternate feature. 2)Button to turn off the traffic lights

--- src/js/components/Semaforo.jsx
+++ src/js/components/Semaforo.jsx
@@ -3,23 +3,48 @@ import React, { useState } from "react";
 const Semaforo = () => {
 
     // Active light useState
-    const [activeLight, setActiveLight] = useState("red")
+    const [activeLight, setActiveLight] = useState("red");
+
+    // Lights
+    const lightsList = ["red", "yellow", "green"];
+    const lightsJSX = lightsList.map((light, key) => <button key={key} onClick={() => setActiveLight(light)} className="light" style={{ backgroundColor: light, boxShadow: (activeLight === light ? "0px 0px 25px 12px gold" : "none") }}></button>
+    )
+
+    // Add purple light
+    const [purpleLightOn, setPurpleLightOn] = useState(false);
 
     // Lights altern function
     const alternateLights = () => {
-        if (activeLight === "red") setActiveLight("green");
-        else if (activeLight === "green") setActiveLight("yellow");
-        else setActiveLight("red");
+        if (purpleLightOn) {
+            if (activeLight === "red") setActiveLight("purple");
+            else if (activeLight === "purple") setActiveLight("green");
+            else if (activeLight === "green") setActiveLight("yellow");
+            else setActiveLight("red");
+        }
+        else {
+            if (activeLight === "red") setActiveLight("green");
+            else if (activeLight === "green") setActiveLight("yellow");
+            else setActiveLight("red");
+        }
     }
 
     return (
         <div className="container">
-            <div className="traffic-lights">
-                <button onClick={()=>setActiveLight("red")} className="light" style={{ backgroundColor: "red", boxShadow: (activeLight === "red" ? "0px 0px 25px 12px gold" : "none")}}></button>
-                <button onClick={()=>setActiveLight("yellow")} className="light" style={{ backgroundColor: "yellow", boxShadow: (activeLight === "yellow" ? "0px 0px 25px 12px gold" : "none")}}></button>
-                <button onClick={()=>setActiveLight("green")} className="light" style={{ backgroundColor: "green", boxShadow: (activeLight === "green" ? "0px 0px 25px 12px gold" : "none")}}></button>
+            <div className="traffic-lights-box">
+                <div className="stick"></div>
+                <div className="traffic-lights">
+                    {lightsJSX}
+                    <button onClick={() => setActiveLight("purple")} className="light"
+                        style={{
+                            backgroundColor: "purple",
+                            boxShadow: (activeLight === "purple" ? "0px 0px 25px 12px gold" : "none"),
+                            display: (purpleLightOn ? "block" : "none")
+                        }}></button>
+                </div>
             </div>
             <button onClick={alternateLights} className="button">Alternate</button>
+            <button onClick={()=>setPurpleLightOn(!purpleLightOn)} className="button">Add/Quit purple light</button>
+            <button onClick={()=>setActiveLight(null)} className="button">Turn off lights</button>
         </div>
     )
 
